dc0dc16c87c814aa1aee174fbccaf69a5732c9d8
Ryan Hung <ryanhungryan@gmail.com>
2025-02-08 15:14:00 +0000

    small fix

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("input your birth year");
+        System.out.println("input your birth year:");
         int birth = scanner.nextInt();
         int year = Year.now().getValue();
-        int age = birth- year;
-        System.out.println("You are: " + age + "years old");
+        int age = year - birth;
+        System.out.println("You are: " + age + " years old");
     }
 }
